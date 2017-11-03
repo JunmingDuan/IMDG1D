40b9fa8a40b42de512884e6074bc891aef5d0d86
jimmy <973842075@qq.com>
2017-11-03 17:37:46 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Using DG solves 1D conservation laws implicitly with positivity-preserving
+limiter.
---
+++ main.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file main.cpp
+ * @brief Implicit DG for 1D conservation laws
+ * @author Duan Junming, duanjm@pku.edu.cn
+ * @version 1.0
+ * @date 2017-11-03
+ */
+
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+  if(argc < 2) {
+    std::cout << "Usage: <>" << std::endl;
+  }
+
+  return 0;
+}
+
